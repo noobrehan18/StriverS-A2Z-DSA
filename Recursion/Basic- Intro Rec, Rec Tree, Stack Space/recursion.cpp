efb85178f3dc24d26ed634f484e5e40b613f911d
Md Rehan <rehan.md0107@gmail.com>
2025-08-09 20:31:08 +0530

    9-aug

---
+++ Recursion/Basic- Intro Rec, Rec Tree, Stack Space/recursion.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+// avoid to write infinite recursion
+// void print(){
+//     cout<<1<<endl;
+//     print(); // stack overflow
+// }
+
+// recursion
+
+void recursion(int count = 0){
+    if (count==4) return;
+    cout<<count<<endl;
+    count++;
+    recursion(count);
+    
+}
+
+
+int main(){
+    // print();
+    recursion();
+    return 0;
+}
