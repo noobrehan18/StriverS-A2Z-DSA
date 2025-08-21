43b1fa153296af64c0c84dcbff6f76fa7ced5b13
Md Rehan <rehan.md0107@gmail.com>
2025-08-21 10:43:52 +0530

    easy-array-q2

--- Arrays/Easy/second-largest-element.cpp
+++ Arrays/Easy/second-largest-element.cpp
@@ -11,7 +11,9 @@ Output: -1
 Explanation: The only value in nums is 10, so there is no second largest value, thus -1 is returned*/
 
 //sheet solution-
-
+#include <bits/stdc++.h>
+#include<iostream>
+using namespace std;
 
 class Solution {
    public:
@@ -29,3 +31,74 @@ class Solution {
     }    
 };
 
+
+//optimal approach->
+
+int secondLarge(int arr[], int n)
+{
+    int largest = arr[0];
+    int sLargest = -1;
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] > largest)
+        {
+            sLargest = largest;
+            largest = arr[i];
+        }
+        else if (arr[i] < largest && arr[i] > sLargest)
+        {
+            sLargest = arr[i];
+        }
+    }
+    return sLargest;
+}
+
+int secondSmall(int arr[], int n)
+{
+    int smallest = arr[0];
+    int secondSmallest = INT_MAX;
+
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] < smallest)
+        {
+            secondSmallest = smallest;
+            smallest = arr[i];
+        }
+        else if (arr[i] != smallest && arr[i] < secondSmallest)
+        {
+            secondSmallest = arr[i];
+        }
+    }
+    return secondSmallest;
+}
+
+void find_secondLargest(int arr[], int n)
+{
+    int secondLargest = secondLarge(arr, n);
+    int secondSmallest = secondSmall(arr, n);
+    cout << "Second Largest: " << secondLargest << endl;
+    cout << "Second Smallest: " << secondSmallest << endl;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    if (n < 2) 
+    {
+        cout << "Array must have at least 2 elements!" << endl;
+        return 0;
+    }
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    find_secondLargest(arr, n);
+
+    return 0;
+}
