b3134afa822543d65195bbbf893c91a491d5fb0d
Md Rehan <rehan.md0107@gmail.com>
2025-08-22 01:00:30 +0530

    MOVE 0(SHEET Q_DONE)

--- Arrays/Easy/move-zero.cpp
+++ Arrays/Easy/move-zero.cpp
@@ -29,3 +29,33 @@ moveZero(arr,n);
     }
     cout<<endl;
 }
+
+/* ///////// SHEET QS->
+
+Move Zeros to End
+
+Given an integer array nums, move all the 0's to the end of the array. The relative order of the other elements must remain the same. This must be done in place, without making a copy of the array.
+Examples:
+Input: nums = [0, 1, 4, 0, 5, 2]
+Output: [1, 4, 5, 2, 0, 0]
+Explanation: Both the zeroes are moved to the end and the order of the other elements stay the same
+Input: nums = [0, 0, 0, 1, 3, -2]
+Output: [1, 3, -2, 0, 0, 0]
+Explanation: All 3 zeroes are moved to the end and the order of the other elements stay the same*/
+
+//SOLUTION------>>>
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int pos = 0; 
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                nums[pos++] = nums[i];
+            }
+        }
+        while (pos < nums.size()) {
+            nums[pos++] = 0;
+        }
+    }
+};
