6b4565a10e4c061723179e74534f059a67572e74
Md Rehan <rehan.md0107@gmail.com>
2025-10-09 00:35:25 +0530

    SHEET QS-> on LENGHT OF LL UISNG TRaVERSaKL

--- LINKED_LIST/Lec 1 - Learn 1D LinkedList/length-of-LL.cpp
+++ LINKED_LIST/Lec 1 - Learn 1D LinkedList/length-of-LL.cpp
@@ -36,3 +36,33 @@ int length(Node *head) {
   }
   return cnt;
 }
+
+//sheet qs-> 
+/*Find the length of the Linked List
+
+
+0
+
+100
+You are given the head of a singly linked list. Your task is to return the number of nodes in the linked list.
+Examples:
+Input: head = [1, 2, 3, 4, 5]
+Output: 5
+Input: head = [8, 6]
+Output: 2
+*/
+
+//soltion->
+
+class Solution {
+public:
+    int getLength(ListNode* head) {
+        int cnt=0;
+        ListNode*temp=head;
+        while(temp){
+            temp=temp->next;
+            cnt++;
+        }
+        return cnt;
+    }
+};
