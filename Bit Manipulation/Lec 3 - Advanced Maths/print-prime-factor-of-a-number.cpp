eecd88a0b69e1fdb3d8e4fc5f216c05414e472c2
Md Rehan <rehan.md0107@gmail.com>
2025-12-22 10:32:26 +0530

    adv_maths 1q done

---
+++ Bit Manipulation/Lec 3 - Advanced Maths/print-prime-factor-of-a-number.cpp
@@ -0,0 +1,18 @@
+vector<int> AllPrimeFactors(int N) {
+    vector<int> ans;
+    
+    for (int i = 2; i * i <= N; i++) {
+        if (N % i == 0) {
+            ans.push_back(i);
+            while (N % i == 0) {
+                N = N/i;
+            }
+        }
+    }
+
+    if (N != 1) {
+        ans.push_back(N);
+    }
+
+    return ans;
+}
