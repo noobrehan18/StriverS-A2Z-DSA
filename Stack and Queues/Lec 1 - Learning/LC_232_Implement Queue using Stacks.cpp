5a9b604bea59c944fbc5e96b5ea22938a81d1c9c
Md Rehan <rehan.md0107@gmail.com>
2025-12-23 17:52:32 +0530

    2 lc done

---
+++ Stack and Queues/Lec 1 - Learning/LC_232_Implement Queue using Stacks.cpp
@@ -0,0 +1,56 @@
+/*Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
+
+Implement the MyQueue class:
+
+void push(int x) Pushes element x to the back of the queue.
+int pop() Removes the element from the front of the queue and returns it.
+int peek() Returns the element at the front of the queue.
+boolean empty() Returns true if the queue is empty, false otherwise.
+Notes:
+
+You must use only standard operations of a stack, which means only push to top, peek/pop from top, size, and is empty operations are valid.
+Depending on your language, the stack may not be supported natively. You may simulate a stack using a list or deque (double-ended queue) as long as you use only a stack's standard operations.
+ 
+
+Example 1:
+
+Input
+["MyQueue", "push", "push", "peek", "pop", "empty"]
+[[], [1], [2], [], [], []]
+Output
+[null, null, null, 1, 1, false]
+*/
+class MyQueue {
+private:
+    stack<int>inst,outst;
+public:
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        inst.push(x);
+    }
+    
+    int pop() {
+        int front=peek();
+        outst.pop();
+        return front;
+    }
+    
+    int peek() {
+        if(outst.empty()){
+            while(!inst.empty()){
+                outst.push(inst.top());
+                inst.pop();
+            }
+        }
+        return outst.top();
+        
+    }
+    
+    bool empty() {
+        return inst.empty() && outst.empty();
+        
+    }
+};
