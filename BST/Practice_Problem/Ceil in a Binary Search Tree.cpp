f9ea2a6bdc1ba6bbd0812aa620bd7b5bd0cf2c67
Md Rehan <rehan.md0107@gmail.com>
2025-12-28 23:06:39 +0530

    ceil in bst

---
+++ BST/Practice_Problem/Ceil in a Binary Search Tree.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    int findCeil(Node* root, int x) {
+        int ceil=-1;
+        while(root){
+            if(root->data== x){
+                ceil=root->data;
+                return ceil;
+            }
+            if(x>root->data){
+                root=root->right;
+            }
+            else{
+                ceil=root->data;
+                root=root->left;
+            }
+        }
+        return ceil;
+        
+
+    }
+};
