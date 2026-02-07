7b2c242a3fb2074ff87b812d15adaac77f26aa33
Md Rehan <rehan.md0107@gmail.com>
2026-02-07 23:44:19 +0530

    Top K Frequent Elements

---
+++ Heaps/hard/LC_347_Top K Frequent Elements.cpp
@@ -0,0 +1,33 @@
+/*Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,1,2,2,3], k = 2
+
+Output: [1,2]*/
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        int n=nums.size();
+        unordered_map<int,int>mp;
+        for(int i=0;i<n;i++){
+            mp[nums[i]]++;
+        }
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> h;
+        for(auto i=mp.begin();i!=mp.end();i++){
+            h.push({i->second,i->first});
+            if(h.size()>k){
+                h.pop();
+            }
+        }
+        vector<int>ans;
+        while(!h.empty()){
+            ans.push_back(h.top().second);
+            h.pop();
+        }
+        return ans;
+    }
+};
