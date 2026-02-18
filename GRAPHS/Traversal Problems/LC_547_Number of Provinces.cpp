a4731754c130c0c38fae9723ce8acf5b353b4ecf
Md Rehan <rehan.md0107@gmail.com>
2026-02-19 00:56:43 +0530

    Number of Provinces

---
+++ GRAPHS/Traversal Problems/LC_547_Number of Provinces.cpp
@@ -0,0 +1,42 @@
+/*There are n cities. Some of them are connected, while some are not. If city a is connected directly with city b, and city b is connected directly with city c, then city a is connected indirectly with city c.
+
+A province is a group of directly or indirectly connected cities and no other cities outside of the group.
+
+You are given an n x n matrix isConnected where isConnected[i][j] = 1 if the ith city and the jth city are directly connected, and isConnected[i][j] = 0 otherwise.
+
+Return the total number of provinces.
+
+ 
+
+Example 1:
+
+
+Input: isConnected = [[1,1,0],[1,1,0],[0,0,1]]
+Output: 2*/
+
+class Solution {
+public:
+    void dfs(int node, vector<vector<int>>& isConnected, vector<int>& vis) {
+        vis[node] = 1;
+
+        for (int j = 0; j < isConnected.size(); j++) {
+            if (isConnected[node][j] == 1 && !vis[j]) {
+                dfs(j, isConnected, vis);
+            }
+        }
+    }
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<int> vis(n, 0);
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                cnt++;
+                dfs(i, isConnected, vis);
+            }
+        }
+        return cnt;
+    }
+};
