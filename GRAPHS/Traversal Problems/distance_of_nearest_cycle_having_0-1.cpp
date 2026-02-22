fdfb502b8703889127844d8fc7976147aaf9d366
Md Rehan <rehan.md0107@gmail.com>
2026-02-23 02:10:38 +0530

    01 Matrix

---
+++ GRAPHS/Traversal Problems/distance_of_nearest_cycle_having_0-1.cpp
@@ -0,0 +1,58 @@
+/*Given an m x n binary matrix mat, return the distance of the nearest 0 for each cell.
+
+The distance between two cells sharing a common edge is 1.
+
+ 
+
+Example 1:
+
+
+Input: mat = [[0,0,0],[0,1,0],[0,0,0]]
+Output: [[0,0,0],[0,1,0],[0,0,0]]*/
+
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+
+        queue<pair<pair<int,int>, int>> q;
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        vector<vector<int>> dist(n, vector<int>(m, 0));
+
+        // Step 1: push all 0s into queue
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(mat[i][j] == 0){
+                    q.push({{i,j},0});
+                    vis[i][j] = 1;
+                }
+            }
+        }
+
+        int delrow[] = {-1,0,1,0};
+        int delcol[] = {0,1,0,-1};
+
+        // Step 2: BFS
+        while(!q.empty()){
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int steps = q.front().second;
+            q.pop();
+
+            dist[row][col] = steps;
+
+            for(int i = 0; i < 4; i++){
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i];
+
+                if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && !vis[nrow][ncol]){
+                    vis[nrow][ncol] = 1;
+                    q.push({{nrow,ncol}, steps+1});
+                }
+            }
+        }
+
+        return dist;
+    }
+};
