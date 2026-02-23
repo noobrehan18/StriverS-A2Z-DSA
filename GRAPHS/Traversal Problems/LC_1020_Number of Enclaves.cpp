94ab63f1017fffb606d928e8156caeae1f8c9386
Md Rehan <rehan.md0107@gmail.com>
2026-02-23 20:55:08 +0530

    Number of Enclaves

---
+++ GRAPHS/Traversal Problems/LC_1020_Number of Enclaves.cpp
@@ -0,0 +1,60 @@
+/*You are given an m x n binary matrix grid, where 0 represents a sea cell and 1 represents a land cell.
+
+A move consists of walking from one land cell to another adjacent (4-directionally) land cell or walking off the boundary of the grid.
+
+Return the number of land cells in grid for which we cannot walk off the boundary of the grid in any number of moves.
+
+ 
+
+Example 1:
+
+
+Input: grid = [[0,0,0,0],[1,0,1,0],[0,1,1,0],[0,0,0,0]]
+Output: 3
+Explanation: There are three 1s that are enclosed by 0s, and one 1 that is not enclosed because its on the boundary.*/
+class Solution {
+public:
+    int numEnclaves(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        queue<pair<int, int>> q;
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 || i==n-1 || j==0 || j==m-1){
+                    if(grid[i][j]==1){
+                        q.push({i,j});
+                        vis[i][j]=1;
+                    }
+                }
+            }
+        }
+        int delrow[]={-1,0,1,0};
+        int delcol[]={0,1,0,-1};
+        while(!q.empty()){
+            int row=q.front().first;
+            int col=q.front().second;
+            q.pop();
+            for(int i=0;i<4;i++){
+                int nrow=row+delrow[i];
+                int ncol=col+delcol[i];
+                if (nrow >= 0 && ncol >= 0 && nrow < n && ncol < m && !vis[nrow][ncol] && grid[nrow][ncol] == 1){
+                    q.push({nrow,ncol});
+                    vis[nrow][ncol]=1;
+                }
+                
+                
+
+            }
+        }
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1 && !vis[i][j]){
+                    cnt++;
+                }
+            }
+        }
+        return cnt;   
+    }
+};
