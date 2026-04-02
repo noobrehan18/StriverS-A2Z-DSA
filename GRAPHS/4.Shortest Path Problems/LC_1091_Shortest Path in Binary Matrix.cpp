3ab1b6ae8aa12ba87397d4d9d641c3ec74551f23
Md Rehan <rehan.md0107@gmail.com>
2026-04-03 01:16:30 +0530

    Shortest Path in binary matrix

--- GRAPHS/4.Shortest Path Problems/LC_1091_Shortest Path in Binary Matrix.cpp
+++ GRAPHS/4.Shortest Path Problems/LC_1091_Shortest Path in Binary Matrix.cpp
@@ -10,3 +10,39 @@ Example 1:
 Input: grid = [[0,1],[1,0]]
 Output: 2   */
 
+
+
+class Solution {
+public:
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        if (grid[0][0]==1) return -1;
+        int m=grid.size();
+        int n=grid[0].size();
+        queue<pair<int,pair<int,int>>> q;
+        vector<vector<int>> dist(m,vector<int>(n,1e9));
+        dist[0][0]=1;
+        q.push({1,{0,0}});
+        while(!q.empty()){
+            int dis=q.front().first;
+            int r=q.front().second.first;
+            int c=q.front().second.second;
+            q.pop();
+
+            if(r==m-1 && c==n-1) return dis;
+
+            for(int i=-1;i<=1;i++){
+                for(int j=-1;j<=1;j++){
+                    int nrow=r+i;
+                    int ncol=c+j;
+                    if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && 
+                        grid[nrow][ncol]==0 && dis+1< dist[nrow][ncol]){
+                            dist[nrow][ncol]=dis+1;
+                            q.push({dis +1 ,{nrow,ncol}});
+                        } 
+                }
+            }
+        }
+        return -1;
+        
+    }
+};
