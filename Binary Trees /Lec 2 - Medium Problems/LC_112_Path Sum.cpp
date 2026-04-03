6c7e388f1481a640c8ea27f7d637e9c3665a4c40
Md Rehan <rehan.md0107@gmail.com>
2026-04-03 23:36:43 +0530

    1 btree qs : Path Sum

---
+++ Binary Trees /Lec 2 - Medium Problems/LC_112_Path Sum.cpp
@@ -0,0 +1,37 @@
+/*Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+
+A leaf is a node with no children.
+
+ 
+
+Example 1:
+
+
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+Output: true*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(root==nullptr){
+            return false;
+        }
+        if(root->left== nullptr && root->right==nullptr){
+            return targetSum==root->val;
+        }
+        return hasPathSum(root->left,targetSum - root->val) || 
+            hasPathSum(root->right, targetSum- root->val);
+
+    }
+};
