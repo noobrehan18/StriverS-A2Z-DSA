f816ef008a9fb151846e23bb423b3b2240d3c6df
Md Rehan <rehan.md0107@gmail.com>
2026-04-12 15:12:23 +0530

    space optimization code for Maximum sum of non-adjacent elements

--- Dynamic Programming/2. 1D DP/Maximum Sum of Non-Adjacent Elements.cpp
+++ Dynamic Programming/2. 1D DP/Maximum Sum of Non-Adjacent Elements.cpp
@@ -52,3 +52,22 @@ int maximumNonAdjacentSum(vector<int> &nums) {
 
     return dp[n - 1];
 }
+//space optimization approach      //tc:O(n) sc:O(1)
+int maximumNonAdjacentSum(vector<int> &nums) {
+    int n = nums.size();
+    int prev = nums[0];
+    int prev2 = 0;
+    for (int i = 1; i < n; i++) {
+        int take = nums[i];
+        if (i > 0) {
+            take += prev2;
+        }
+        int nottake = prev;
+
+        int curi = max(take, nottake);
+
+        prev2 = prev;
+        prev = curi;
+    }
+    return prev;
+}
