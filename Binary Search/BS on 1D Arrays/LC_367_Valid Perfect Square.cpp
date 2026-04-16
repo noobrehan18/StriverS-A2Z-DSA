a38ba774402030a87935fa01bedf41438d057ee2
Md Rehan <rehan.md0107@gmail.com>
2026-04-16 23:19:19 +0530

    BS ESY--Valid Perfect Square

---
+++ Binary Search/BS on 1D Arrays/LC_367_Valid Perfect Square.cpp
@@ -0,0 +1,29 @@
+/*
+
+Topics
+premium lock icon
+Companies
+Given a positive integer num, return true if num is a perfect square or false otherwise.
+
+A perfect square is an integer that is the square of an integer. In other words, it is the product of some integer with itself.
+
+You must not use any built-in library function, such as sqrt.
+
+ 
+
+Example 1:
+
+Input: num = 16
+Output: true */
+
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int i = 1;
+        while (num > 0) {
+            num -= i;
+            i += 2;
+        }
+        return num == 0;
+    }
+};
