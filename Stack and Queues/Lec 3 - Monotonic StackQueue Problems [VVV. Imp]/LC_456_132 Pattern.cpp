1c51daee33851f54be2c210b6c74a6b125a526d8
Md Rehan <rehan.md0107@gmail.com>
2026-05-18 22:26:16 +0530

    Stack-med-456. 132 Pattern

---
+++ Stack and Queues/Lec 3 - Monotonic StackQueue Problems [VVV. Imp]/LC_456_132 Pattern.cpp
@@ -0,0 +1,34 @@
+/*Given an array of n integers nums, a 132 pattern is a subsequence of three integers nums[i], nums[j] and nums[k] such that i < j < k and nums[i] < nums[k] < nums[j].
+
+Return true if there is a 132 pattern in nums, otherwise, return false.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: false
+Explanation: There is no 132 pattern in the sequence.
+*/
+
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+        int n=nums.size();
+        stack<int>st;
+        int third=INT_MIN;
+        for(int i=n-1;i>=0;i--){
+            if(nums[i]<third){
+                return true;
+            }
+        
+            while(!st.empty() && nums[i] > st.top() ){
+                third=st.top();
+                st.pop();
+            }
+
+            st.push(nums[i]);
+        }
+        return false; 
+    }
+};
