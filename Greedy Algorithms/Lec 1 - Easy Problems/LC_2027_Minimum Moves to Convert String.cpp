34f24b9ce86eb6b6ff442beb053e07b7b8a82555
Md Rehan <rehan.md0107@gmail.com>
2026-05-20 22:37:29 +0530

    greedy-esy-2027. Minimum Moves to Convert String

---
+++ Greedy Algorithms/Lec 1 - Easy Problems/LC_2027_Minimum Moves to Convert String.cpp
@@ -0,0 +1,30 @@
+/*You are given a string s consisting of n characters which are either 'X' or 'O'.
+
+A move is defined as selecting three consecutive characters of s and converting them to 'O'. Note that if a move is applied to the character 'O', it will stay the same.
+
+Return the minimum number of moves required so that all the characters of s are converted to 'O'.
+
+ 
+
+Example 1:
+
+Input: s = "XXX"
+Output: 1 */
+
+class Solution {
+public:
+    int minimumMoves(string s) {
+        int moves=0;
+        int n=s.size();
+        for(int i=0;i<n;){
+            if(s[i]=='X'){
+            moves++;
+            i+=3;
+            }
+            else{
+                i++;
+            }
+        }
+        return moves;
+    }
+};
