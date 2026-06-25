3d541204d2a533ba98470cc58d9883aaf1e0d980
Md Rehan <rehan.md0107@gmail.com>
2026-06-25 22:23:11 +0530

    string-esy-28. Find the Index of the First Occurrence in a String-

---
+++ strings/Easy/LC_28_Find the Index of the First Occurrence in a String.cpp
@@ -0,0 +1,25 @@
+/*Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+ 
+
+Example 1:
+
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0 */
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int n = haystack.size();
+        int m = needle.size();
+        for (int i = 0; i <= n - m; i++) {
+            int j = 0;
+            while (j < m && haystack[i + j] == needle[j]) {
+                j++;
+            }
+            if (j == m)
+                return i;
+        }
+        return -1;
+    }
+};
