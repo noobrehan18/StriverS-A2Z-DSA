a90f70a3460540089a069616a64330e553487e03
Md Rehan <rehan.md0107@gmail.com>
2026-07-03 22:16:20 +0530

    string-esy-14. Longest Common Prefix

---
+++ strings/Easy/LC_14_Longest Common Prefix.cpp
@@ -0,0 +1,26 @@
+/*Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+ 
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl" */
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string prefix = strs[0];
+        for (int i = 1; i < strs.size(); i++) {
+            while (strs[i].find(prefix) != 0) {
+                prefix.pop_back();
+
+                if (prefix.empty())
+                    return "";
+            }
+        }
+        return prefix;
+    }
+};
