e7c38afefa247848c869c6b73d391c32d43dcb64
Md Rehan <rehan.md0107@gmail.com>
2026-07-15 17:20:41 +0530

    maths-esy-3658. GCD of Odd and Even Sums

---
+++ Maths/LC_3658_GCD of Odd and Even Sums.cpp
@@ -0,0 +1,16 @@
+/*You are given an integer n. Your task is to compute the GCD (greatest common divisor) of two values:
+sumOdd: the sum of the smallest n positive odd numbers.
+sumEven: the sum of the smallest n positive even numbers.
+Return the GCD of sumOdd and sumEven.
+
+Example 1:
+Input: n = 4
+Output: 4
+*/
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        return n;
+        
+    }
+};
