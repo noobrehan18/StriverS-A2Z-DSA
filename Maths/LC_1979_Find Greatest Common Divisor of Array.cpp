668b23a9dfb41e87c0a40e46c2503a76ae9e5ba1
Md Rehan <rehan.md0107@gmail.com>
2026-07-18 19:42:55 +0530

    maths-esy-1979. Find Greatest Common Divisor of Array

---
+++ Maths/LC_1979_Find Greatest Common Divisor of Array.cpp
@@ -0,0 +1,28 @@
+/*Given an integer array nums, return the greatest common divisor of the smallest number and largest number in nums.
+
+The greatest common divisor of two numbers is the largest positive integer that evenly divides both numbers.
+
+ 
+
+Example 1:
+
+Input: nums = [2,5,6,9,10]
+Output: 2 */
+class Solution {
+public:
+    int gcd(int a, int b) {
+        while (b != 0) {
+            int t = b;
+            b = a % b;
+            a = t;
+        }
+        return a;
+    }
+
+    int findGCD(vector<int>& nums) {
+        int mn = *min_element(nums.begin(), nums.end());
+        int mx = *max_element(nums.begin(), nums.end());
+
+        return gcd(mn, mx);
+    }
+};
